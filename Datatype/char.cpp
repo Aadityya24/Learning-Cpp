36645162d215cb60e74134b8a62326fd1115a48b
Aadityya24 <aadityyaggholasgaon@gmail.com>
2025-03-02 18:44:51 +0530

    Updated <file-name> with changes

---
+++ Datatype/char.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch = 'a';
+     
+    cout << ch << endl; //to print character
+    cout << (int) ch << endl; //using type conversion to print int value
+}
